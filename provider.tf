f82ad2c486c4703f1898edfce1adc5852b7cd19d
sravankumarsra <144658877+sravankumarsra@users.noreply.github.com>
2024-01-02 11:01:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,4 @@
-terraform {
- provider "aws" {
-   region = "us-east-1"
-   }
-
- backend "s3" {
-   bucket = "tf-tuts-state"
-   key    = "state"
+provider "aws" {
    region = "us-east-1"
  }
-}
 
