f2fe8b96bbddb84c82658916b29701d0eb864967
sravankumarsra <144658877+sravankumarsra@users.noreply.github.com>
2024-01-02 10:58:02 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,12 @@
 terraform {
- required_providers {
-   aws = {
-     source  = "hashicorp/aws"
-     version = "~> 4.18.0"
+ provider "aws" {
+   region = "us-east-1"
    }
- }
 
  backend "s3" {
    bucket = "tf-tuts-state"
    key    = "state"
-   region = "eu-central-1"
+   region = "us-east-1"
  }
 }
 
